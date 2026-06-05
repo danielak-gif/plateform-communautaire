11bccd78e2b8b6091abaed5c730d2378d4c25d1a
Daniel Akondé <akondedaniel27@gmail.com>
2026-06-05 10:33:51 +0100

    change page_presentation

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,10 @@ Route::middleware('auth')->group(function () {
 
 Route::get('/annuaire', [CommunauteController::class, 'index'])->name('communaute.index');
 Route::view('/projet-8', 'projet8')->name('projet8');
+// Aliases and redirects to support English/alternate URLs
 Route::redirect('/projet8', '/projet-8');
+Route::redirect('/project-8', '/projet-8');
+Route::redirect('/project8', '/projet-8');
 
 Route::middleware('auth')->group(function () {
     Route::get('/annuaire/soumettre', [CommunauteController::class, 'create'])->name('communaute.create');
